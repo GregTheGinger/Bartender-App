9f76f9ff99e74fc0bd039cf17a7e120a2ca070e1
Greg Cunningham <n01064060@ospreys.unf.edu>
2018-08-28 22:27:57 -0400

    Adjuected shared views to update based off what html page the user is on

--- Bartender App/Controllers/HomeController.cs
+++ Bartender App/Controllers/HomeController.cs
@@ -22,6 +22,13 @@ namespace Bartender_App.Controllers
             return View();
         }
 
+        public IActionResult Cart()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
